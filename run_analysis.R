d918a0ab9c36fe5392205d1661bcbc94331f3d5b
Tim Grove <tagrove@ameritech.net>
2015-06-18 20:39:31 -0400

    Added codebook, updated README, worked on project

--- run_analysis.R
+++ run_analysis.R
@@ -7,13 +7,13 @@ setwd("~/GitHub/Getting-and-Cleaning-Data/")
 # Step 1
 # Merge the training and test sets to create one data set
 #-------------------------------------------------------------------------------------------------------
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 # create 'x' data set
 x_data <- rbind(x_train, x_test)
@@ -29,7 +29,7 @@ subject_data <- rbind(subject_train, subject_test)
 # Extract only the measurements on the mean and standard deviation for each measurement
 #-------------------------------------------------------------------------------------------------------
 
-features <- read.table("features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 
 # get only columns with mean() or std() in their names
 mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
@@ -45,10 +45,24 @@ names(x_data) <- features[mean_and_std_features, 2]
 # Use descriptive activity names to name the activities in the data set
 #-------------------------------------------------------------------------------------------------------
 
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name
+names(y_data) <- "activity"
+
 #-------------------------------------------------------------------------------------------------------
 # Step 4
 # Appropriately label the data set with descriptive variable names
 #-------------------------------------------------------------------------------------------------------
+
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
 
 #-------------------------------------------------------------------------------------------------------
 # Step 5
@@ -56,7 +70,12 @@ names(x_data) <- features[mean_and_std_features, 2]
 # for each activity and each subject
 #-------------------------------------------------------------------------------------------------------
 
+# 66 <- 68 columns but last two (activity & subject)
+tidy_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
 #-------------------------------------------------------------------------------------------------------
 # Step 6
 # Write the tidy data set to the working directory
 #-------------------------------------------------------------------------------------------------------
+
+write.table(tidy_data, "tidy_data.txt", row.name=FALSE)
