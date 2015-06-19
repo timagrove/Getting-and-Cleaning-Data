d61666a61312fdf087c40abd3599e46742b47508
Tim Grove <tagrove@ameritech.net>
2015-06-18 20:15:58 -0400

    Add Data for project
    Add data for Project and Create
    run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+# Set working Directory
+
+setwd("~/GitHub/Getting-and-Cleaning-Data/")
+
+# Load Required Library
+library(plyr)
+
+# Step 1
+# Merge the training and test sets to create one data set
+
+# Step 2
+# Extract only the measurements on the mean and standard deviation for each measurement
+
+# Step 3
+# Use descriptive activity names to name the activities in the data set
+
+# Step 4
+# Appropriately label the data set with descriptive variable names
+
+# Step 5
+# Create a second, independent tidy data set with the average of each variable
+# for each activity and each subject
+
+# Step 6
+# Write the tidy data set to the working directory
